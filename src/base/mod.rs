0f119a3d65ee089e3021b596a1098c20bc01f18a
sb952468655 <952468655@qq.com>
2018-02-08 17:18:01 +0800

    add euclidean algorithm

--- src/base/mod.rs
+++ src/base/mod.rs
@@ -1,3 +1,11 @@
-pub fn say() {
-    println!("hi");
+
+pub fn euclidean_algorithm(p: i32, q: i32) -> i32 {
+    match q {
+        0 => p,
+        _ => {
+            let r = p % q;
+            println!("p = {}, q = {}, p % q = {}", p, q, r);
+            euclidean_algorithm(q, r)
+        },
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ extern crate algorithms_with;
 use algorithms_with::base;
 
 fn main() {
-    base::say();
+    let v = base::euclidean_algorithm(12, 30);
+    println!("gcd = {}",v);
 }
