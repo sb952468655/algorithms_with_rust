17820b683a24aa355c7253d74db3e915fb443171
sb952468655 <952468655@qq.com>
2018-02-20 17:24:27 +0800

    add shell sort

--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -1,7 +1,6 @@
 pub fn selection_sort(a: &mut [i32]) {
     for i in 0..a.len()-1 {
         let mut min = i;
-        println!("i = {}",i);
         for j in (i+1)..a.len() {
             if a[j] < a[min] {
                 min = j;
@@ -21,6 +20,22 @@ pub fn insertion_sort(a: &mut [i32]) {
     }
 }
 
+pub fn shell_sort(a: &mut [i32]) {
+    let n = a.len();
+    let mut h = 1;
+    while h < n/3 { h = 3*h + 1; }
+    while h >= 1 {
+        for i in h..n {
+            for j in (h..i+1).filter(|s| {
+                *s >= h && (i - *s)% h == 0 
+            }).rev() {
+                if a[j] < a[j - h] { exchange(a, j, j-h); }
+            }
+        }
+        h = h/3;
+    }
+}
+
 fn exchange(a: &mut [i32], i: usize, j: usize) {
     let temp = a[i];
     a[i] = a[j];
@@ -35,11 +50,19 @@ mod tests {
         let mut a = [3,5,8,2,7,9,23,11,16,78];
         selection_sort(&mut a);
         assert_eq!([2,3,5,7,8,9,11,16,23,78], a);
-    }  
+    } 
+
     #[test]
     fn test_insertion_sort() {
         let mut a = [3,5,8,2,7,9,23,11,16,78];
         insertion_sort(&mut a);
-        assert_eq!([2,3,5,7,8,9,11,16,23,78], a);
-    }    
+        assert_eq!([2,3,5,7,8,9,11,16,23,78], a);                                                                                              
+    }
+
+    #[test]
+    fn test_shell_sort() {
+        let mut a = [3,5,8,2,7,9,23,11,16,78];
+        shell_sort(&mut a);
+        assert_eq!([2,3,5,7,8,9,11,16,23,78], a);                                                                                              
+    }      
 }
